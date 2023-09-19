1c8a06da7a29af1e661e7a5cf16f1c0ccfd1cdb1
Katherine Yee <129905433+katherineeyee@users.noreply.github.com>
2023-09-19 16:11:24 -0700

    Add files via upload

---
+++ Partition.java
@@ -0,0 +1,61 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class Partition {
+	
+	public static int partitionLomuto(int[] arr,int low, int high) {
+		int pivot = arr[high];
+		int i = low - 1;
+		
+		for(int j = low;j<high;j++) {
+			if (arr[j]<=pivot) {
+				i = i+1;
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+
+		arr[high] = arr[i+1];
+		arr[i+1]=pivot;
+		return i+1;
+				   
+
+	}
+	
+	/***********ANSWER QUESTION HERE*****************/
+    /*
+     * What sort of input arrays will enable Hoare’s algorithm to still create relatively 
+     * equal size partitions whereas Lumoto’s algorithm will create unequal partitions?
+     */
+	
+	public static int partitionHoare(int[] arr,int low, int high) {
+		int pivot = arr[low];
+		int i = low-1;
+		int j = high+1;
+		while(true){
+			i = i+1;
+			j = j-1;
+			while(arr[j]>pivot) {
+				j = j-1;
+			}
+			while(arr[i]<pivot) {
+				i = i +1;
+			}
+			if(i<j) {
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;	
+			}
+			else {
+				return j;
+			}
+			
+		}
+		
+	}
+	
+	
+
+}
